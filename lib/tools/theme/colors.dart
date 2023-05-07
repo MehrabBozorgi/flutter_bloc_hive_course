a8c8d1b329ce4ea25473ac767983466cb31d02cf
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2023-05-07 14:25:10 +0430

    bloc_hive_course

---
+++ lib/tools/theme/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+final primaryColor = Colors.purple.shade200;
+const backGroundColor = Colors.white;
+const textFieldColor = Color(0xFFF1F2F4);
+
+const darkColor = Color(0xff3D3F47);
+const redColor = Color(0xffea504c);
+const borderColor = Color(0xFF979ba3);
+
+const whiteColor = Colors.white;
+final greyColor = Colors.grey.shade300;
